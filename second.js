3b662a639ff87bf88a28ae824681501e473101a1
Ashish Kumar <electroashish111@gmail.com>
2025-07-01 16:41:47 +0530

    Create second.js

---
+++ second.js
@@ -0,0 +1,36 @@
+// QUES 2:- get product price using a promise
+
+const products = {
+  'apple': 1.2,
+  'banana': 0.5,
+  'orange': 0.8
+};
+
+function getProductPrice(productId) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      if (products.hasOwnProperty(productId)) {
+        resolve(products[productId]);
+      } else {
+        reject("Product not found");
+      }
+    }, 1000); 
+  });
+}
+
+
+getProductPrice('banana')
+  .then(function(price) {
+    console.log("Price:", price);
+  })
+  .catch(function(error) {
+    console.log("Error:", error);
+  });
+
+getProductPrice('mango')
+  .then(function(price) {
+    console.log("Price:", price);
+  })
+  .catch(function(error) {
+    console.log("Error:", error);
+  });
